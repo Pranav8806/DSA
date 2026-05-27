cd6e706970dbf90b6062fbd9caf01decf0696859
Pranav Sharma <pranavps8806@gmail.com>
2026-05-27 12:44:44 +0530

    optimal solution to problem n queens added

---
+++ 51. N-Queens/code.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    public:
+        bool isValidPos(vector<string> &board,int row,int col,int n){
+            //check for row
+            for(int i=0;i<n;i++){
+                if(board[row][i]=='Q') return false;
+            }
+            //check for column
+            for(int i=0;i<n;i++){
+                if(board[i][col]=='Q') return false;
+            }
+            //check for left diagonal
+            for(int i=row, j=col;i>=0 && j>=0;i--,j--){
+                if(board[i][j]=='Q') return false;
+            }
+            //check for right diagoanl
+            for(int i=row,j=col;i>=0 && j<n;i--,j++){
+                if(board[i][j]=='Q') return false;
+            }
+        return true;        
+        }
+        void nqueens(vector<string> &board,int row,int n,vector<vector<string>>&ans){
+            if(row==n) {
+                ans.push_back({board});
+                return;
+            }
+            for(int i=0;i<n;i++){
+                if(isValidPos(board,row,i,n)){
+                    board[row][i]='Q';
+                    nqueens(board,row+1,n,ans);
+                    board[row][i]='.';
+                }
+            }
+        }
+        vector<vector<string>> solveNQueens(int n) {
+            vector<vector<string>>ans;
+            vector<string>board(n,string(n,'.'));
+            nqueens(board,0,n,ans);
+            return ans;
+        }
+    };
