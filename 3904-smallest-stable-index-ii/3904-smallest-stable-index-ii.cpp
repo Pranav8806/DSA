35785106716e9e6efca2399c01e36eef80e149c6
Pranav Sharma <109479050+Pranav8806@users.noreply.github.com>
2026-09-05 13:35:56 +0530

    Time: 12 ms (51.64%), Space: 210.9 MB (15.41%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        int n = nums.size();
-        int mnindx=INT_MAX;
-        vector<int> prefixMax(n), suffixMin(n);
-        
-        prefixMax[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            prefixMax[i] = max(prefixMax[i - 1], nums[i]);
+        int n=nums.size();
+        vector<int>pref(n),suff(n);
+        pref[0]=nums[0];
+        suff[n-1]=nums[n-1];
+        for(int i=1;i<n;i++){
+            pref[i]=max(pref[i-1],nums[i]);
         }
-        
-        suffixMin[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suffixMin[i] = min(suffixMin[i + 1], nums[i]);
+        for(int i=n-2;i>=0;i--){
+            suff[i]=min(suff[i+1],nums[i]);
         }
-        
-        for (int i = 0; i < n; i++) {
-            if (prefixMax[i] - suffixMin[i] <= k) {
-                mnindx=min(mnindx,i);
-                return mnindx;
-            }
+        for(int i=0;i<n;i++){
+            if(pref[i]-suff[i]<=k) return i;
         }
-        
         return -1;
     }
 };
