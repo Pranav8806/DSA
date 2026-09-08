5e94e6edf7d493258fa388cc714f65795e6f3c6b
Pranav Sharma <109479050+Pranav8806@users.noreply.github.com>
2026-09-08 16:10:34 +0530

    Time: 8 ms (31.41%), Space: 112.1 MB (52.13%) - LeetHub

--- 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
+++ 1524-number-of-sub-arrays-with-odd-sum/1524-number-of-sub-arrays-with-odd-sum.cpp
@@ -2,18 +2,15 @@ class Solution {
 public:
     int numOfSubarrays(vector<int>& nums) {
         int n=nums.size();
-        vector<int>PrefixSum(n);
-        PrefixSum[0]=nums[0];
-        for(int i=1;i<n;i++){
-            PrefixSum[i]=PrefixSum[i-1]+nums[i];
-        }
+        long long prefsum=0;
         long long cnt=0;
         long long even=0;
         long long odd=0;
         long long mod=1000000007;
-        for(int j=0;j<n;j++){
-            if(PrefixSum[j]%2 !=0) odd++;
-            else even++;   
+        for(int i=0;i<n;i++){
+            prefsum+=nums[i];
+            if(prefsum%2 !=0) odd++;
+            else even++; 
         }
         cnt+=odd;
         //difference of one odd and one even subarray;
